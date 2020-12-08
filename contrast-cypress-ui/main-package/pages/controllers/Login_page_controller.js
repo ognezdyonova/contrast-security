b0a65a862b00bf8305252779b5eaf93f73e52a7d
Olga Gnezdyonova <olga.gnezdyonova@centralreach.com>
2020-12-08 08:01:05 -0500

    Original commit

--- contrast-cypress-ui/main-package/pages/controllers/Login_page_controller.js
+++ contrast-cypress-ui/main-package/pages/controllers/Login_page_controller.js
@@ -4,5 +4,21 @@ class Login_page_controller {
     constructor() {
         this.elements = new Login_page_objects();
     }
+
+    login_user(login, pass) {
+
+        let login_page = new Login_page_controller();
+        login_page.elements.login_input().should("be.visible").type(login);
+        login_page.elements.next_button().should("be.visible").click({force: true});
+        login_page.elements.pass_input().should("be.visible").type(pass);
+        login_page.elements.login_button().should("be.visible").click({force: true});
+        Cypress.Cookies.preserveOnce('session_id', 'remember_token');
+        cy.on('uncaught:exception', (err) => {
+            expect(err.message).to.include('The following error originated from your application code');
+            return false;
+        });
+
+
+    }
 }
 export default Login_page_controller;
--- contrast-cypress-ui/main-package/pages/objects/Vulnerabilities_page_objects.js
+++ contrast-cypress-ui/main-package/pages/objects/Vulnerabilities_page_objects.js
@@ -15,6 +15,22 @@ class Vulnerabilities_page_objects {
         return cy.get('[data-testid="contrast-grid-row-0"] [data-testid="grid-cell"]');
     }
 
+    filter(tag) {
+        return cy.get('[data-e2e="checkbox-' + tag + '"]')
+    }
+
+    vul_btn(){
+        return cy.get('[data-e2e="filter-icon-title_vulnerability"]')
+    }
+        
+    title(){
+        return cy.get('[data-e2e="grid-header-name"]');
+    }
+
+    clear(){
+        return cy.get('[data-e2e="column-header-title_vulnerability"] a')
+    }
+
     add_tag_to_item_button(){
         return cy.get('[data-testid="action_tag"]');
     }
